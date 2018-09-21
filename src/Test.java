cf0f520c453a8e567b748d0818d2c50dc49b06ec
Mithoon Kumar <mithoonk@zeta.tech>
2018-09-21 12:02:10 +0530

    Use of supplyAsync to return the result of compeletable future

--- src/Test.java
+++ src/Test.java
@@ -2,13 +2,14 @@ import java.util.concurrent.*;
 //Runnable has no return type
 public class Test {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(()->{
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 throw new IllegalStateException(e);
             }
             System.out.println("Hello World");
+            return "some text";
         });
         System.out.println("Executing the main method");
         System.out.println("Priting the result of the completable future " + completableFuture.get());
