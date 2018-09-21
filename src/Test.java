cc9eb1aefe99a2ecd5c6657af71c9e82992e22d4
Mithoon Kumar <mithoonk@zeta.tech>
2018-09-21 14:59:52 +0530

    Use of thenCombine

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,11 @@ public class Test {
 
     public static CompletableFuture<String> getUserName() {
         return CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Inside the name task");
            return "name";
         });
@@ -11,18 +16,36 @@ public class Test {
 
     public static CompletableFuture<Integer> getUserAge() {
         return CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Inside the age task");
             return 20;
         });
     }
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
-            System.out.println("Inside completable future");
-            return "Yes";
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 20;
         });
-        CompletableFuture<Integer> futureCompletableFuture = getUserName().thenCompose((arg)->{
-            return getUserAge();
+        CompletableFuture<Integer>completableFuture1 = CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 10;
         });
-        System.out.println("Final outcome " + futureCompletableFuture.get());
+
+        CompletableFuture<Integer> completableFuture2 = completableFuture1.thenCombine(completableFuture, (first, second )->{
+            return first * second;
+        });
+        System.out.println("Printing the output " + completableFuture2.get() );
     }
 }
