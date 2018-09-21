a448c17daeb67aa74ca0bcee340ac46577172a6d
Mithoon Kumar <mithoonk@zeta.tech>
2018-09-21 17:14:03 +0530

    Use of anyOf

--- src/Test.java
+++ src/Test.java
@@ -1,38 +1,39 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.*;
-import java.util.stream.Collectors;
 
-//Runnable has no return type
 public class Test {
 
-    public static CompletableFuture<Integer> processData(Integer i) {
-        return CompletableFuture.supplyAsync(()->{
+    public static void main(String [] args) throws ExecutionException, InterruptedException {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
             try {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Inside the processData method");
-            return i;
+            return "first future completed";
         });
+
+        CompletableFuture<String> completableFuture1 = CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "second future completed";
+        });
+
+        CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "third future completed";
+        });
+
+        CompletableFuture<Object> completableFuture3 = CompletableFuture.anyOf(completableFuture, completableFuture1, completableFuture2);
+        System.out.println(completableFuture3.get());
     }
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        List<Integer> data = Arrays.asList(1,2,3,4);
 
-        List<CompletableFuture<Integer>> dataFutures = data.stream()
-                .map(datum -> processData(datum))
-                .collect(Collectors.toList());
-        for(int i = 0; i < dataFutures.size(); i++) {
-            System.out.println("Printing " + dataFutures.get(i).get());
-        }
-        CompletableFuture<Void> allFutures  = CompletableFuture.allOf(dataFutures.toArray(new CompletableFuture[dataFutures.size()]));
-        CompletableFuture<List<Integer>> allProcessedData = allFutures.thenApply((v)->{
-           return dataFutures.stream().map(datumFuture -> datumFuture.join()).collect(Collectors.toList());
-        });
-        System.out.println("Printing the result of all the futures " + allProcessedData.get());
-        System.out.println("Hello");
-        System.out.println("Getting the result of all futures " + allFutures.get()) ;
-    }
+
 }
