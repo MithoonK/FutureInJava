408ac1bd0db1efa153ab89825a91334becdcd8a3
Mithoon Kumar <mithoonk@zeta.tech>
2018-09-21 18:15:48 +0530

    Use of handle

--- src/Test.java
+++ src/Test.java
@@ -3,15 +3,19 @@ import java.util.concurrent.*;
 public class Test {
 
     public static void main(String [] args) throws ExecutionException, InterruptedException {
-        int age = -1;
+        int age = 1;
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
             if (age < 0) {
                 throw new IllegalArgumentException("Age can not be negative");
             } else {
                 return "ok";
             }
-        }).exceptionally(ex ->{
-            return "Exception";
+        }).handle((res, ex) ->{
+            if (res == null) {
+                return "Exception occured";
+            } else {
+                return "no exception";
+            }
         });
         System.out.println("Printing the outcome " + completableFuture.get());
     }
