fe932b624212636bcb7f77c299a05dbcf108e3e3
Mithoon Kumar <mithoonk@zeta.tech>
2018-09-21 12:05:52 +0530

    Use of our own thread pool to execute the tasks

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ import java.util.concurrent.*;
 //Runnable has no return type
 public class Test {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Executor executor = Executors.newFixedThreadPool(10);
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
             try {
                 TimeUnit.SECONDS.sleep(1);
@@ -10,7 +11,7 @@ public class Test {
             }
             System.out.println("Hello World");
             return "some text";
-        });
+        }, executor);
         System.out.println("Executing the main method");
         System.out.println("Priting the result of the completable future " + completableFuture.get());
         System.out.println("Completed the main method   ");
