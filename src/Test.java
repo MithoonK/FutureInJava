6d4de06a339f964092fdeadc10f169f484c31fd2
Mithoon Kumar <mithoonk@zeta.tech>
2018-09-21 11:51:46 +0530

    lambda expression is not executed until completableFuture.get() is called

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,16 @@ import java.util.concurrent.*;
 //Runnable has no return type
 public class Test {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CompletableFuture<String> completableFuture = new CompletableFuture<String>();
-        completableFuture.complete("Manual result");
-        System.out.println(completableFuture.get());
+        CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(()->{
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            System.out.println("Hello World");
+        });
+        System.out.println("Executing the main method");
+        System.out.println("Priting the result of the completable future ");
+        System.out.println("Completed the main method   ");
     }
 }
